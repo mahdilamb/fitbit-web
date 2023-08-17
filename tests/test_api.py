7df2535cb2552f2d894b810dab50d3d0dde60e9b
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-17 11:49:19 +0100

    add chrome

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,15 @@
+import requests
 from fitbit_web import auth
 
-
+import webbot
 def env_test():
     """Test that the secrets have been set."""
     auth.check_secrets()
 
+def auth_test():
+    scopes = ["profile"]
+
+    auth.get_tokens_local(scopes=scopes)
 
 if __name__ == "__main__":
     import sys
